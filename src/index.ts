7520503749b722ab4e1ef3425d39944ffc4b96fe
Elisee-dev <esabidani@gmail.com>
2024-04-03 09:51:06 +0000

    Start user's creation

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import express, { Response, Request } from "express";
 import cors from "cors";
 import "dotenv/config";
+import myUserRoute from "./routes/MyUserRoute";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/test', async (req: Request, res: Response) => {
-    res.json({ message: "Hello!" })
-});
+app.get('/api/user', myUserRoute);
 
 app.listen(7000, () => {
     console.log("Server started on localhost:7000");
-
 })
