854c69725350776c87ad35aba229833168c6b620
Elisee-des <esabidani@gmail.com>
2024-04-12 12:15:53 +0000

    Ok pour le test

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express, { Response, Request } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/user', myUserRoute);
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log('Connect to database'));
 
+app.use("/api/my/user", myUserRoute);
 app.listen(7000, () => {
     console.log("Server started on localhost:7000");
 })
